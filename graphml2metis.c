894fc0fc43ded8f9b5020507367973f67e992dd5
Marcos Oliveira <moliveirajun2013@my.fit.edu>
2015-08-02 11:07:10 -0400

    file and exceptions handling

--- graphml2metis.c
+++ graphml2metis.c
@@ -16,24 +16,26 @@ void export_metis(const igraph_t* graph) {
 	igraph_vit_t adjacent_vertex_iterator;
 	igraph_integer_t nodes_number;
 	igraph_integer_t edges_number;
+
 	// edges selector
 	igraph_es_all(&edge_selector, IGRAPH_EDGEORDER_ID);
 	igraph_es_size(graph, &edge_selector, &edges_number);
 	igraph_es_destroy(&edge_selector);
+
 	// vertices selector
 	igraph_vs_all(&vertex_selector);
 	igraph_vs_size(graph, &vertex_selector, &nodes_number);
 	printf("%li %li\n", (long int) nodes_number, (long int) edges_number);
-	// vertice iterator
+
+	// vertices iterator
 	igraph_vit_create(graph, vertex_selector, &vertex_iterator);
+
 	while (!IGRAPH_VIT_END(vertex_iterator)) {
-		igraph_vs_adj(&adjacent_vertex_selector,
-				(long int) IGRAPH_VIT_GET(vertex_iterator), IGRAPH_ALL);
-		igraph_vit_create(graph, adjacent_vertex_selector,
-				&adjacent_vertex_iterator);
+		igraph_vs_adj(&adjacent_vertex_selector, (long int) IGRAPH_VIT_GET(vertex_iterator), IGRAPH_ALL);
+		igraph_vit_create(graph, adjacent_vertex_selector, &adjacent_vertex_iterator);
 		while (!IGRAPH_VIT_END(adjacent_vertex_iterator)) {
-			printf("%li ",
-					((long int) IGRAPH_VIT_GET(adjacent_vertex_iterator) + 1));
+			// igraph indexes starting from 0, we need it starting from 1
+			printf("%li ", ((long int) IGRAPH_VIT_GET(adjacent_vertex_iterator) + 1));
 			IGRAPH_VIT_NEXT(adjacent_vertex_iterator);
 		}
 		igraph_vit_destroy(&adjacent_vertex_iterator);
@@ -45,22 +47,41 @@ void export_metis(const igraph_t* graph) {
 	igraph_vs_destroy(&vertex_selector);
 }
 
+int read_graph_and_export_metis(const char * graph_file_name) {
+	FILE* input = fopen(graph_file_name, "r");
+	int ret = -1;
+	if (input != 0) {
+		igraph_t graph;
+		ret = igraph_read_graph_graphml(&graph, input, 0);
+		if (ret == IGRAPH_SUCCESS) {
+			export_metis(&graph);
+			igraph_destroy(&graph);
+		}
+	}
+	return ret;
+}
+
 int main(int argc, char **argv) {
 	FILE * input = 0;
+	int ret = -1;
 	if (argc < 2) {
 		printf("You need to give me the graphml filename!\n");
 	} else {
-		input = fopen(argv[1], "r");
-		if (input != 0) {
-			igraph_t graph;
-			igraph_read_graph_graphml(&graph, input, 0);
-			export_metis(&graph);
-			igraph_destroy(&graph);
-			fclose(input);
-		} else {
-			printf("I could not open '%s'\n", argv[1]);
+		ret = read_graph_and_export_metis(argv[1]);
+		switch (ret) {
+			case -1:
+				printf("I could not open '%s'\n", argv[1]);
+				break;
+			case IGRAPH_PARSEERROR:
+				printf("I could not understand '%s', parser error.\n", argv[1]);
+				break;
+			case IGRAPH_UNIMPLEMENTED:
+				printf("I can not understand '%s', there is no support to graphml reading.\n", argv[1]);
+				break;
+			default:
+				break;
 		}
 	}
-	return 0;
+	return ret;
 }
 
